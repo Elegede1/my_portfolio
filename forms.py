4fbcbee33be3e1036eb35b10c6a6d2509c4a309a
Jekuthiel <jekuthielnnamdi@gmail.com>
2025-07-06 06:03:19 +0100

    Changing database to NoSql, uploading a new profile photo and other minor problem that required annoying major fixing.

--- forms.py
+++ forms.py
@@ -1,34 +1,50 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from flask_wtf.file import FileField, FileAllowed
-from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo, URL
-from wtforms import ValidationError
-from flask_ckeditor import CKEditor, CKEditorField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo, URL, Optional
+from flask_ckeditor import CKEditorField
 
 
 class LoginForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+
+
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
+
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=8, message="Password must be at least 8 characters long."),
+        Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$',
+               message="Password must contain at least one uppercase letter, one lowercase letter, and one number.")
+    ])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    # def validate_email(self, field):
-    #     if User.query.filter_by(email=field.data).first():
-    #         raise ValidationError('Email already registered.')
-    #
-    # def validate_username(self, field):
-    #     if User.query.filter_by(username=field.data).first():
-    #         raise ValidationError('Username already in use.')
+
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     body = CKEditorField('Content', validators=[DataRequired()])
-    img_url = FileField('Image', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif'], 'Images only!')])
-    submit = SubmitField('Post')
+
+    # WORLD-CLASS CHANGE: Set a generic label in the form definition
+    img_url = FileField('Project Image', validators=[
+        Optional(),
+        FileAllowed(['jpg', 'png', 'jpeg', 'gif'], 'Images only!')
+    ])
+
+    github_url = StringField('GitHub URL', validators=[URL(), Optional()])  # Added Optional() here, see below
+    submit = SubmitField('Save Changes')
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    subject = StringField('Subject', validators=[DataRequired()])
+    message = CKEditorField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send')
